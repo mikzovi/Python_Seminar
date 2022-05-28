8c41ccf7e1f667fa9c4050500d1320358a84ddd5
mikzovi <mikzovi@ya.ru>
2022-05-28 17:43:29 +0300

    Home work. Seminar02 Task01 Sum ood index added.

--- HW_Seminar02/HW_Task_S02_01_SumOdd.py
+++ HW_Seminar02/HW_Task_S02_01_SumOdd.py
@@ -1,14 +1,15 @@
 # 1.	Найти сумму чисел списка стоящих на нечетной позиции
 # Пример:[1,2,3,4] -> 4
 
-origin_list = [1,2,3,4]
+origin_list = [1, 2, 3, 4]
+
 
 def sum_odd_index(current_list):
     length_current_list = len(current_list)
     sum = 0
-    for i in range(0,length_current_list,2):
+    for i in range(0, length_current_list, 2):
         sum += current_list[i]
     return sum
 
-print(f'Сумма чисел списка {origin_list}, стоящих на нечетной позиции: {sum_odd_index(origin_list)}')
 
+print(f'Сумма чисел списка {origin_list}, стоящих на нечетной позиции: {sum_odd_index(origin_list)}')

71adf1f1e082a8e68ce0ad00c16a9348483e45c1
mikzovi <mikzovi@ya.ru>
2022-05-31 21:22:19 +0300

    Home work. Seminar03 Task01 NOK lowest total multiple added.

--- HW_Seminar03/HW_Task_S03_01_NOK.py
+++ HW_Seminar03/HW_Task_S03_01_NOK.py
@@ -5,6 +5,7 @@ number_y = int(input("Введите второе число: "))
 
 
 def nok_calculate(current_number_x, current_number_y):
+    if current_number_x * current_number_y == 0: return("не существует")
     if current_number_x > current_number_y:
         bigger_number = current_number_x
     else:
@@ -12,9 +13,8 @@ def nok_calculate(current_number_x, current_number_y):
     while(True):
         if (bigger_number % current_number_x == 0) and (bigger_number % current_number_y == 0):
             nok = bigger_number
-            break
+            return nok
         bigger_number += 1
-    return nok
-
+    
 print(
     f"Наименьшее общее кратное чисел {number_x} и {number_y}: {nok_calculate(number_x,number_y)}")
